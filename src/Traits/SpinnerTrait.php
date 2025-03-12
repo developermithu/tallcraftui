6b80a4e0a4dce2500986a453142f37608daa85f2
developermithu <developermithu@gmail.com>
2025-03-13 02:03:13 +0600

    Add `HasColorAttributes` trait for reusability

--- src/Traits/SpinnerTrait.php
+++ src/Traits/SpinnerTrait.php
@@ -2,36 +2,11 @@
 
 namespace Developermithu\Tallcraftui\Traits;
 
+use Developermithu\Tallcraftui\Traits\Colors\HasColorAttributes;
+
 trait SpinnerTrait
 {
-    public array $colorAttributes = [
-        'primary',
-        'secondary',
-        'black',
-        'white',
-        'slate',
-        'gray',
-        'zinc',
-        'neutral',
-        'stone',
-        'red',
-        'orange',
-        'amber',
-        'yellow',
-        'lime',
-        'green',
-        'emerald',
-        'teal',
-        'cyan',
-        'sky',
-        'blue',
-        'indigo',
-        'violet',
-        'purple',
-        'fuchsia',
-        'pink',
-        'rose',
-    ];
+    use HasColorAttributes;
 
     public function getColorClasses(): string
     {
